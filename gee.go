f4e074bca00b155f67b3b70aed571328419d26ff
wangshunzhang <wang719695@gmail.com>
2018-10-18 16:51:55 +0800

    结构优化

--- gee.go
+++ gee.go
@@ -2,47 +2,75 @@ package gee
 
 import (
 	"fmt"
+	"github.com/julienschmidt/httprouter"
 	"net/http"
 )
 
+// 控制器接口
+// 方法、结构体都可以实现此方法
+type GeeHandlerInterface interface {
+	ServeHTTP(c *Context)
+}
+
+// 使func()实现GeeHandlerInterface接口
+type adaptFuncToGeeHandler func(*Context)
+
+func (f adaptFuncToGeeHandler) ServeHTTP(c *Context) {
+	f(c)
+}
+
+// 路由节点
 type Endpoint struct {
 	// http方法
-	Verb string
+	Method string
 	// 路径
 	Path string
 	// 控制器
-	Handler GeeHandler
-	HandlerFunc func(*Context)
-	isFunc bool
+	Handler GeeHandlerInterface
 }
 
-// 引擎，内部包含很多Endpoint
+// 引擎
 type Engine struct {
 	Endpoints *[]Endpoint
+	Router *httprouter.Router
 }
+
+// 上下文
+type Context struct {
+	ResponseWriter http.ResponseWriter
+	Request *http.Request
+}
+
+// *** public methods ***
 
 // 创建一个引擎
 func CreateEngine() (Engine){
-	return Engine{Endpoints: &( []Endpoint{} )}
+	return Engine{
+		Endpoints: &([]Endpoint{}),
+		Router: httprouter.New(),
+	}
 }
 
 // 注册路由
 // 如果要修改结构体，这里e用地址传递
-func (e *Engine) Register(verb string, path string, handler GeeHandler) {
+func (e *Engine) Register(method string, path string, handler GeeHandlerInterface) {
 	handlers := append(*(e.Endpoints), Endpoint{
-		Verb: verb,
+		Method: method,
 		Path: path,
 		Handler: handler,
 	})
 	e.Endpoints = &handlers
+
+	// TODO 使用httprouter来匹配路由
+	// e.Router.Handle(method, path, handler)
 }
 
-func (e *Engine) RegisterFunc(verb string, path string, fn func(*Context)) {
+// 注册路由（方法）
+func (e *Engine) RegisterFunc(method string, path string, fn func(*Context)) {
 	handlers := append(*(e.Endpoints), Endpoint{
-		Verb: verb,
+		Method: method,
 		Path: path,
-		HandlerFunc: fn,
-		isFunc: true,
+		Handler: adaptFuncToGeeHandler(fn),
 	})
 	e.Endpoints = &handlers
 }
@@ -56,28 +84,12 @@ func (e *Engine) Post(path string, handlerFunc func(*Context)) {
 	e.RegisterFunc(http.MethodPost, path, handlerFunc)
 }
 
-
-// 上下文
-type Context struct {
-	ResponseWriter http.ResponseWriter
-	Request *http.Request
-}
-
-// 控制器接口
-type GeeHandler interface {
-	ServeHTTP(c *Context)
-}
-
 // 处理请求
-func (c *Context) Handle(e Engine) {
+func (c *Context) Handle(e *Engine) {
 	// 路径解析
 	for _, ep := range *e.Endpoints {
-		if c.Request.URL.Path == ep.Path && c.Request.Method == ep.Verb {
-			if ep.isFunc == true {
-				ep.HandlerFunc(c)
-			} else {
-				ep.Handler.ServeHTTP(c)
-			}
+		if c.Request.URL.Path == ep.Path && c.Request.Method == ep.Method {
+			ep.Handler.ServeHTTP(c)
 			return
 		}
 	}
@@ -89,15 +101,14 @@ func (c *Context) Handle(e Engine) {
 // 实现http.Handler，将RequestWriter和Request转换为Context
 func (e Engine) ServeHTTP(rw http.ResponseWriter, req *http.Request) {
 	c := Context{rw, req}
-	c.Handle(e)
+	c.Handle(&e)
 }
 
 // 启动服务
 func (e Engine) Start(addr string) (error) {
-	// r := mux.NewRouter()
 	for _, v := range *e.Endpoints {
-		fmt.Println(fmt.Sprintf("Server Register route: %s %s", v.Verb, v.Path))
-		// r.Handle(v.Path, v.Handler).Methods(v.Verb)
+		fmt.Println(fmt.Sprintf("Server Register route: %s %s", v.Method, v.Path))
+		// r.Handle(v.Path, v.Handler).Methods(v.Method)
 	}
 	// http.Handle("/", r)
 	http.Handle("/", e)
--- test/main.go
+++ test/main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"github.com/Pingze-github/gee"
 	"net/http"
+	"time"
 )
 
 type HomeController struct{}
@@ -15,12 +16,18 @@ func foo(c *gee.Context) {
 	c.ResponseWriter.Write([]byte("Gee~ by HandlerFunc"))
 }
 
+func timeoutHandler(c *gee.Context) {
+	time.Sleep(time.Duration(10e9))
+	c.ResponseWriter.Write([]byte("Done"))
+}
+
 func main() {
 	homeController := HomeController{}
 
 	engine := gee.CreateEngine()
 	engine.Register(http.MethodGet, "/", homeController)
 	engine.Get("/func", foo)
+	engine.Get("/timeout", timeoutHandler)
 	// engine.Get("/hello", homeController)
 	engine.Start("127.0.0.1:8080")
 }
